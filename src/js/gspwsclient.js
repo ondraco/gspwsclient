8b556c097c2bf60a740ade6ed3f20ac838125ecf
ondra <ondraco@gmail.com>
2021-01-15 17:29:39 +0100

    GSP protocol

--- src/js/gspwsclient.js
+++ src/js/gspwsclient.js
@@ -1,21 +1,56 @@
 function GspWs(url, key) {
   let _this = this;
-  var socket;
-  var ws_key;
+  let socket;
+  let authOk = false;
 
-  this.connect = function() {
-    socket = new WebSocket(url);
-    ws_key = key;
+  const MsgId = {
+    Authorize: 1,
+    Get: 2,
+    Set: 3,
+    Sub: 4,
+  };
+
+  this.connect = function () {
+    socket = new WebSocket(url, "gsp-protocol");
+    {
+      binaryType: "arraybuffer";
+    }
 
     socket.addEventListener("open", onOpen);
     socket.addEventListener("message", onMsg);
+    socket.addEventListener("close", onClose);
+    socket.addEventListener("error", onError);
+  };
+
+  function doAuth() {
+    let headerLen = 8;
+    let keySize = key.length * 2;
+
+    var arr = new ArrayBuffer(headerLen + keySize);
+    var view = new Uint16Array(arr, 0, 2);
+    var keyView = new Uint16Array(arr, headerLen, key.length);
+
+    view[0] = MsgId.Authorize;
+    view[1] = key.length;
+
+    for (var i = 0, strLen = key.length; i < strLen; i++) {
+      keyView[i] = key.charCodeAt(i);
+    }
+
+    socket.send(arr);
   }
 
   function onOpen(event) {
-    socket.send("Hello Server!");
+    doAuth();
   }
 
-  function onMsg(event) {
-    console.log("Message from server ", event.data);
+  function onMsg(event) {}
+
+  function onClose(event) {
+    console.log(event);
+  }
+
+  function onError(event) {
+    console.log(event);
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 function test() {
-  let host = "wss://127.0.0.1:443/API";
-  let gws = new GspWs(host);
+  let host = 'wss://127.0.0.1:443/API';
+  let key = 'rOG6t8kqkyY=';
+  let gws = new GspWs(host, key);
   gws.connect(host);
 }
