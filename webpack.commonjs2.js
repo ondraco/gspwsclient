4a064ca937af6fd14693b66d341b674ecf887295
ondra <ondraco@gmail.com>
2021-01-29 11:13:01 +0100

    non minimized version added

--- webpack.commonjs2.js
+++ webpack.commonjs2.js
@@ -10,10 +10,25 @@ module.exports = (env) => {
       optimization: {
         minimize: true,
       },
-      externals: ['bufferutil', 'utf-8-validate'],
+      externals: ["bufferutil", "utf-8-validate"],
       output: {
         path: path.resolve(__dirname, "release"),
         filename: "commonjs2/gspclient.min.js",
+        library: "GSPClient",
+        libraryTarget: "commonjs2",
+      },
+    },
+    {
+      target: "node",
+      entry: [path.resolve(__dirname, "src/index.js")],
+      mode: "production",
+      optimization: {
+        minimize: false,
+      },
+      externals: ["bufferutil", "utf-8-validate"],
+      output: {
+        path: path.resolve(__dirname, "release"),
+        filename: "commonjs2/gspclient.js",
         library: "GSPClient",
         libraryTarget: "commonjs2",
       },
